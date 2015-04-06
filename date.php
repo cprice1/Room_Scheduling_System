2b6b4fbbbc423a2f1d8c0b8f1a555361dceab213
cprice1 <cprice1@una.edu>
2015-04-06 04:49:03 -0500

    Date Function
    Date Function for Room Scheduling System

--- date.php
+++ date.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,29 +6,23 @@ and open the template in the editor.
     <body>
         <?php
 
-        function getAvailableTimes($date, $times) {
+        function getAvailableTimes($date) {
 
             //restub with database calls
             if ($date . dayOfWeek == "Monday") {
-                $times = array("8:00 AM", "9:00 AM", "10:00 AM");
-                
+                return array("8:00 AM", "9:00 AM", "10:00 AM");
             } else if ($date . dayOfWeek == "Tuesday") {
-                $times = array("11:00 AM", "12:00 PM", "1:00 PM");
-                
+                return array("11:00 AM", "12:00 PM", "1:00 PM");
             } else if ($date . dayOfWeek == "Wednesday") {
-                $times = array("2:00 PM", "3:00 PM", "4:00 PM");
-                
+                return array("2:00 PM", "3:00 PM", "4:00 PM");
             } else if ($date . dayOfWeek == "Thursday") {
-                $times = array("5:00 PM", "6:00 PM", "7:00PM");
-                
+                return array("5:00 PM", "6:00 PM", "7:00PM");
             } else if ($date . dayOfWeek == "Friday") {
-                $times = array("8:00 AM", "9:00 AM", "10:00 AM");
-                
+                return array("8:00 AM", "9:00 AM", "10:00 AM");
             } else if ($date . dayOfWeek == "Saturday") {
-                $times = array("11:00 AM", "12:00 PM", "1:00 PM");
-                
+                return array("11:00 AM", "12:00 PM", "1:00 PM");
             } else if ($date . dayOfWeek == "Sunday") {
-                $times = array("2:00 PM", "3:00 PM", "4:00 PM");
+                return array("2:00 PM", "3:00 PM", "4:00 PM");
             }
         }
         ?>
